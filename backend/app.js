f43a32fa7ce997abc89cb8d7f1e882c397e549b8
Alexander Oiling <alexander.oiling@helsinki.fi>
2025-05-05 00:40:21 +0300

    added some middleware to the application

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
 const express = require('express')
 const blogsRouter = require('./controllers/blogposts')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 mongoose.set('strictQuery', false)
 
 const app = express()
@@ -19,7 +21,12 @@ mongoose.connect(mongoUrl)
 })
 
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
