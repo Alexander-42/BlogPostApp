265fc3ca51f9321e6696c8d1af8dd9526a7c2c2c
Alexander Oiling <alexander.oiling@helsinki.fi>
2025-05-04 23:50:57 +0300

    moved database connection and express app creation to app.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,6 @@
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const blogsRouter = require('./controllers/blogposts')
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-
-const app = express()
-
-const mongoUrl = config.MONGODB_URI
-
-logger.info('Connecting to ', mongoUrl)
-mongoose.connect(mongoUrl)
-  .then(result => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB', error.message)
-})
-
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
