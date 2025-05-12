9476af85d4694abd26b8595462348fe4ae9ac371
Alexander Oiling <alexander.oiling@helsinki.fi>
2025-05-12 22:37:46 +0300

    Initialized test database and added a test

--- backend/test/blog_api.test.js
+++ backend/test/blog_api.test.js
@@ -1,15 +1,46 @@
-const { test, after} = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach} = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+const initialBlogs = [
+    {
+        title: 'A test blog',
+        author: 'A test author',
+        url: 'A test url',
+        likes: 3
+    },
+    {
+        title: 'A test blog 2',
+        author: 'A test author 2',
+        url: 'A test url 2',
+        likes: 1
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let i = 0; i < initialBlogs.length; i++) {
+        let blogObject = new Blog(initialBlogs[i])
+        await blogObject.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 after(async () => {
