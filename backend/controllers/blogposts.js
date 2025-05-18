6517a4fca7475047c506529cf2133e2bf0a14e4b
Alexander Oiling <alexander.oiling@helsinki.fi>
2025-05-18 23:50:49 +0300

    isolated userExtractor to middleware

--- backend/controllers/blogposts.js
+++ backend/controllers/blogposts.js
@@ -2,25 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const userExtractor = require('../utils/middleware').userExtractor
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', userExtractor, async (request, response, next) => {
   try {  
-    const blogBody = request.body
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: "invalid token"})
-    }
-
-    const user = await User.findById(decodedToken.id)
-
-    if (!user) {
-      return response.status(400).json({ error: 'userId missing or not valid'})
-    }
+    const user = request.user
 
     const blog = new Blog({
       author: blogBody.author,
@@ -41,18 +32,13 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response, next) => {
   try {
-
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: "invalid token"})
-    }
-    console.log(request.params.id)
     const blog = await Blog.findById(request.params.id)
-    const user = await User.findById(decodedToken.id)
-
+    console.log('This is at the controller')
+    console.log(request.user._id.toString())
+    const user = request.user
+    
     if ( blog.user.toString() !== user._id.toString()) {
       return response.status(401).json({error: "Unauthorized user cannot delete posts"})
     }
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -22,6 +24,25 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
+const userExtractor = async (request, response, next) => {
+  const token = request.token
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token"})
+  }
+  console.log(token)
+  const user = await User.findById(decodedToken.id)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid'})
+  }
+  request.user = user
+  console.log('---')
+  console.log(user)
+  console.log('---')
+
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -42,5 +63,6 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
